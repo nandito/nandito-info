ca8a4f115a063b37aa7380cf97031796ef4026c3
Nandor Biro <dev@nandito.info>
2019-09-16 20:55:30 +0200

    Set styles for blog index cover image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -15,47 +16,108 @@ class BlogIndex extends React.Component {
         location={this.props.location}
         title={siteTitle}
         header={() => (
-          <div className="w-full m-0 p-0 bg-cover bg-bottom bg-indigo-600 -mb-32">
+          <div className="w-full -mb-16">
             <div
-              className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal"
-              style={{ maxHeight: '460px' }}
+              className="container flex justify-center items-center w-full max-w-full relative"
+              style={{ height: '45vh'}}
             >
-              <Link className="text-white font-extrabold text-3xl md:text-5xl" to={`/`}>
+              <div className="absolute inset-0 z-10" style={{
+                background: 'linear-gradient(0deg, rgba(0,0,0,0.72) 5%, rgba(1,1,1,0.15) 45%)',
+              }}></div>
+              <div className="absolute inset-0 z-0">
+                <Img
+                  className="h-full"
+                  fluid={data.headerImage.childImageSharp.fluid}
+                />
+              </div>
+
+              <Link
+                className="text-white font-extrabold text-3xl md:text-5xl relative z-20"
+                style={{ textShadow: '1px 1px 2px rgba(1, 1, 1, 0.7)' }}
+                to={`/`}
+              >
                 {siteTitle}
               </Link>
             </div>
 
-            <nav className="mb-16 w-full">
-              <div className="container mx-auto flex items-center">
-                <div className="flex w-1/2 pl-4 text-sm">
-                  <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
-                    <li className="mr-2">
-                      <a className="inline-block py-2 px-2 text-white no-underline hover:underline" href="post.html">POSTS</a>
-                    </li>
-                  </ul>
+            <div className="-mt-32 relative z-30">
+              <nav className="w-full">
+                <div className="container mx-auto flex items-center">
+                  <div className="flex w-1/2 pl-4 text-sm">
+                    <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
+                      <li className="mr-2">
+                        <a
+                          className="inline-block py-2 px-2 text-white no-underline hover:underline"
+                          href="post.html"
+                        >
+                          POSTS
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="flex w-1/2 justify-end content-center">
+                    <a
+                      className="
+                        inline-block text-gray-500 no-underline hover:text-white
+                        hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar
+                      "
+                      data-tippy-content="@twitter_handle"
+                      href="https://twitter.com/intent/tweet?url=#"
+                    >
+                      <svg className="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                        <path
+                          d="
+                            M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0
+                            1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063
+                            4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0
+                            1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25
+                            0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125
+                            2.688 0 5.063-.875 7.188-2.5-1.25
+                            0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125
+                            1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673
+                            5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228
+                            5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938
+                            1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579
+                            5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625
+                            4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313
+                            2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"
+                        />
+                      </svg>
+                    </a>
+
+                    <a
+                      className="
+                        inline-block text-gray-500 no-underline hover:text-white
+                        hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar
+                      "
+                      data-tippy-content="#facebook_id"
+                      href="https://www.facebook.com/sharer/sharer.php?u=#"
+                    >
+                      <svg
+                        className="fill-current h-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 32 32
+                      ">
+                        <path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z" />
+                      </svg>
+                    </a>
+                  </div>
                 </div>
-                <div className="flex w-1/2 justify-end content-center">
-                  <a className="inline-block text-gray-500 no-underline hover:text-white hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar" data-tippy-content="@twitter_handle" href="https://twitter.com/intent/tweet?url=#">
-                    <svg className="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"></path></svg>
-                  </a>
-                  <a className="inline-block text-gray-500 no-underline hover:text-white hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar" data-tippy-content="#facebook_id" href="https://www.facebook.com/sharer/sharer.php?u=#">
-                    <svg className="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z"></path></svg>
-                  </a>
-                </div>
-              </div>
-            </nav>
+              </nav>
+            </div>
           </div>
         )}
       >
         <SEO title="All posts" />
 
-        <div className="flex flex-wrap justify-between pt-12 -mx-6">
+        <div className="flex flex-wrap justify-between pt-12 -mx-6 relative z-20">
           {posts.map(({ node }) => {
             const title = node.frontmatter.title || node.fields.slug
 
             return (
               <article
-                className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink"
+                className="w-full sm:w-1/2 md:w-1/3 p-6 flex flex-col flex-grow flex-shrink"
                 key={node.fields.slug}
               >
                 <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
@@ -65,7 +127,7 @@ class BlogIndex extends React.Component {
                   >
                     <img
                       alt={node.frontmatter.title}
-                      className="h-64 w-full rounded-t pb-6"
+                      className="h-64 w-full rounded-t pb-6 object-cover"
                       src="https://source.unsplash.com/collection/225/800x600"
                     />
 
@@ -88,10 +150,21 @@ class BlogIndex extends React.Component {
                   </Link>
                 </div>
 
-                <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                <div className="
+                  flex-none mt-auto bg-white rounded-b
+                  rounded-t-none overflow-hidden shadow-lg p-6
+                ">
                   <div className="flex items-center justify-between">
-                    <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" />
-                    <p className="text-gray-600 text-xs md:text-sm">{node.timeToRead} MIN READ</p>
+                    <img
+                      className="w-8 h-8 rounded-full mr-4 avatar"
+                      data-tippy-content="Author Name"
+                      src="http://i.pravatar.cc/300"
+                      alt="Avatar of Author"
+                    />
+
+                    <p className="text-gray-600 text-xs md:text-sm">
+                      {node.timeToRead} MIN READ
+                    </p>
                   </div>
                 </div>
               </article>
@@ -107,6 +180,17 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query {
+    headerImage: file(absolutePath: { regex: "/farakas.jpg/" }) {
+      childImageSharp {
+        fluid(
+          maxHeight: 460,
+          maxWidth: 1920,
+          cropFocus: CENTER,
+        ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
     site {
       siteMetadata {
         title
