78efded6cec25971c8759ae32617364b06d97563
Nandor Biro <dev@nandito.info>
2020-04-03 17:27:03 +0200

    Remove initial content from the post header

--- src/components/header.js
+++ src/components/header.js
@@ -17,19 +17,12 @@ const Header = ({ title }) => {
         <div className="flex pl-4 text-sm">
           <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
             <li className="mr-2">
-              <a className="inline-block py-2 px-2 text-white no-underline" href="index.html">HOME</a>
-            </li>
-            <li className="mr-2">
-              <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"
-                href="#">LINK</a>
-            </li>
-            <li className="mr-2">
-              <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"
-                href="#">LINK</a>
-            </li>
-            <li className="mr-2">
-              <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-2"
-                href="#">LINK</a>
+              <Link
+                className="inline-block py-2 px-2 text-white no-underline"
+                to="/"
+              >
+                Home
+              </Link>
             </li>
           </ul>
         </div>
