66c096eeda9c3df78869683a63225fec1b312275
Nandor Biro <dev@nandito.info>
2020-04-03 20:04:38 +0200

    Set up page content types

---
+++ content/pages/uses.md
@@ -0,0 +1,7 @@
+---
+title: Uses
+date: "2020-04-02T18:50:00.000Z"
+description: ""
+---
+
+Test
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,68 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+// TODO: add filter and sorting option
+// TODO: replace allMarkdownRemark queries when the 1st todo is done
+
+/**
+ * Filters the files by source instance name
+ * @param {array} files that we need to filter
+ * @param {string} sourceInstanceName the "name" in the `gatsby-source-filesystem` config
+ * @returns the array of files with the given source instance
+ */
+const filterBySourceInstanceName = (files, sourceInstanceName) => (
+  files.filter(
+    file => {
+      const isMarkdown = file.internal.mediaType === 'text/markdown'
+      const isMatch = file.sourceInstanceName === sourceInstanceName
+
+      return isMarkdown && isMatch
+    }
+  )
+)
+
+/**
+ * Gets all files' MarkdownRemark type equivalent by the file's absoulte path.
+ * @param {array} files where we need to replace `File` types by `MarkdownRemark`
+ * @param {array} markdowns the source of all MarkdownRemark content
+ * @returns the array of markdown remark content
+ */
+const getMarkdownsByFiles = (files, markdowns) => (
+  files.map(
+    file => markdowns.find(md => md.fileAbsolutePath === file.absolutePath)
+  )
+)
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    Query: {
+      allPages: {
+        type: [`MarkdownRemark`],
+        resolve: (source, args, context, info) => {
+          const allFile = context.nodeModel.getAllNodes({ type: `File` })
+          const allMarkdown = context.nodeModel.getAllNodes({ type: `MarkdownRemark` })
+          const pageFiles = filterBySourceInstanceName(allFile, 'pages')
+          const pageMarkdowns = getMarkdownsByFiles(pageFiles, allMarkdown)
+
+          return pageMarkdowns
+        }
+      },
+      allPosts: {
+        type: [`MarkdownRemark`],
+        resolve: (source, args, context, info) => {
+          const allFile = context.nodeModel.getAllNodes({ type: `File` })
+          const allMarkdown = context.nodeModel.getAllNodes({ type: `MarkdownRemark` })
+          const blogFiles = filterBySourceInstanceName(allFile, 'blog')
+          const blogMarkdowns = getMarkdownsByFiles(blogFiles, allMarkdown)
+
+          return blogMarkdowns
+        }
+      }
+    }
+  }
+  createResolvers(resolvers)
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
