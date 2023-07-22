a1da1a9953deb1a6fa7bae6e0fac21c59c6df13e
Nandor Biro <dev@nandito.hu>
2023-07-22 17:55:00 +0200

    fix: update sitemap url

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link } from 'gatsby'
-import Header from './header'
+import { Link } from "gatsby"
+import Header from "./header"
 
 class Layout extends React.Component {
   render() {
@@ -11,35 +11,33 @@ class Layout extends React.Component {
         {header({ title })}
 
         <main className="container flex-auto px-4 md:px-0 max-w-6xl mx-auto">
-          <div className="mx-0 sm:mx-6">
-            {children}
-          </div>
+          <div className="mx-0 sm:mx-6">{children}</div>
         </main>
 
         <footer className="bg-gray-900">
           <div className="container mx-auto flex flex-wrap items-center space-between px-2 py-8">
             <div className="w-full md:w-1/2 text-white text-center md:text-left">
               <span className="text-base text-gray-200">
-                © {new Date().getFullYear()}, Built with
-                {' '}
-                <a className="no-underline hover:underline" href="https://www.gatsbyjs.org">
+                © {new Date().getFullYear()}, Built with{" "}
+                <a
+                  className="no-underline hover:underline"
+                  href="https://www.gatsbyjs.org"
+                >
                   Gatsby
                 </a>
-                {' & '}
-                <a className="no-underline hover:underline" href="https://tailwindcss.com/">
+                {" & "}
+                <a
+                  className="no-underline hover:underline"
+                  href="https://tailwindcss.com/"
+                >
                   Tailwind CSS
                 </a>
               </span>
             </div>
 
-            <ul
-              className="flex-1 flex justify-center md:justify-end mt-3 md:mt-0"
-            >
+            <ul className="flex-1 flex justify-center md:justify-end mt-3 md:mt-0">
               <li>
-                <Link
-                  className="text-white hover:underline mr-4"
-                  to="/rss.xml"
-                >
+                <Link className="text-white hover:underline mr-4" to="/rss.xml">
                   RSS Feed
                 </Link>
               </li>
@@ -47,7 +45,7 @@ class Layout extends React.Component {
               <li>
                 <Link
                   className="text-white hover:underline"
-                  to="/sitemap.xml"
+                  to="/sitemap-index.xml"
                 >
                   Sitemap
                 </Link>
